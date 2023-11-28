88e734dc72b22b1688b3cf3797c5313009a029f9
Nicolly Moreira do Carmo <nicollymoreira840@gmail.com>
2023-11-28 10:28:08 -0300

    instalação do handlebars e do nodemon

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express")
+const exphbs = require("express-handlebars")
 
 const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
 
 app.get('/', (requisicao, resposta) => {
     resposta.send("Òla mundo!")
 })
 
-app.listen(3000,() => {
+app.listen(3000, () => {
     console.log("Servidor rodando a porta 3000!")
 })
