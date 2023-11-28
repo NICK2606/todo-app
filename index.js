21ceb6cbb9110e41ff907652f19f0c4b7a0b088c
Nicolly Moreira do Carmo <nicollymoreira840@gmail.com>
2023-11-28 10:12:09 -0300

    Primeira execução do servidor

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const express = require("express")
+
 const app = express()
+
+app.get('/', (requisicao, resposta) => {
+    resposta.send("Òla mundo!")
+})
+
+app.listen(3000,() => {
+    console.log("Servidor rodando a porta 3000!")
+})
